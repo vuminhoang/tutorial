1af3af8c5a7bbb63ca4eb3a94490e4a4365c35a4
Hoang Vu Minh <150101931+vuminhoang@users.noreply.github.com>
2024-08-22 13:26:06 +0700

    Update hide_githubrepo_app.py

--- hide_githubrepo_app.py
+++ hide_githubrepo_app.py
@@ -1,22 +1,32 @@
 import streamlit as st
 
-def hide_github_icon():
-    hide_github_style = """
-        <style>
-        .viewerBadge_container__1QSob {
-            display: none !important;
-        }
-        </style>
+def hide_streamlit_elements():
+    hide_github_icon = """
+    <style>
+    .css-1jc7ptx, .e1ewe7hr3, .viewerBadge_container__1QSob, 
+    .styles_viewerBadge__1yB5_, .viewerBadge_link__1S137, 
+    .viewerBadge_text__1JaDK { 
+        display: none; 
+    } 
+    #MainMenu { 
+        visibility: hidden; 
+    } 
+    footer { 
+        visibility: hidden; 
+    } 
+    header { 
+        visibility: hidden; 
+    }
+    </style>
     """
-    st.markdown(hide_github_style, unsafe_allow_html=True)
+    st.markdown(hide_github_icon, unsafe_allow_html=True)
 
 def main():
     st.title("My Simple Streamlit App")
-    st.write("This is a simple app with the GitHub icon hidden.")
+    st.write("This is a simple app with custom UI elements hidden.")
 
-    # Call the function to hide the GitHub icon
-    hide_github_icon()
+    # Call the function to hide specific Streamlit elements
+    hide_streamlit_elements()
 
 if __name__ == "__main__":
     main()
-  
